26b2d7d68ab166007728c68ccacdb4088f9b4a6e
eplanas2 <137905610+eplanas2@users.noreply.github.com>
2023-06-29 17:34:49 +0200

    Revert "tutorial test"
    This reverts commit c79b114419aff7e54c174a1deba16b60ed74a97b.

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -96,7 +96,7 @@ function AddItemForm({ onNewItem }) {
                     value={newItem}
                     onChange={e => setNewItem(e.target.value)}
                     type="text"
-                    placeholder="New Item EPP todo list"
+                    placeholder="New Item"
                     aria-describedby="basic-addon1"
                 />
                 <InputGroup.Append>
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add Item to EPP todo list'}
+                        {submitting ? 'Adding...' : 'Add Item'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
